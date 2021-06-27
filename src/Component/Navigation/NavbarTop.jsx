48ede4e17721f3fb21ce15a57658272802b6fcb6
Aamer4ui <aamersohelansari@gmail.com>
2021-06-26 17:23:52 -0700

    some changes

--- src/Component/Navigation/NavbarTop.jsx
+++ src/Component/Navigation/NavbarTop.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import {Navbar,Nav} from "react-bootstrap";
-import  "../Navigation/NavbarTop.css";
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import "../Navigation/NavbarTop.css";
 
 export const NavbarTop = () => {
-    return (
-        <div>
-            <Navbar bg="primary" variant="dark" expand="lg">
-  <Navbar.Brand href="#home">AMR Creation</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto ">
-      <Nav.Link className href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Product</Nav.Link>
-      <Nav.Link href="#link">Services</Nav.Link>
-      <Nav.Link href="#link">About Us</Nav.Link>
-      <Nav.Link href="#link">Contact Us</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">AMR Creation</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto ">
+            <Nav.Link className href="#home">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#link">Product</Nav.Link>
+            <Nav.Link href="#link">Services</Nav.Link>
+            <Nav.Link href="#link">About Us</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 };
-
